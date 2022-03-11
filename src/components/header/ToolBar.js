5308c1980ffa0365f269cb3df84ae76128291f4e
Cristina Lester <cristina.deelester@gmail.com>
2022-03-10 18:49:12 -0800

    added search bar

--- src/components/header/ToolBar.js
+++ src/components/header/ToolBar.js
@@ -16,7 +16,7 @@ const ToolBarContainer = styled.div`
     content: "";
     width: 2px;
     background: #f5f5f5;
-    height: 40px;
+    height: 50px;
     position: absolute;
     right: 0;
     display: inline-block;
--- src/components/layout/split-screen/LeftPanel.js
+++ src/components/layout/split-screen/LeftPanel.js
@@ -1,15 +1,40 @@
 import React from "react";
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import { IconButton, InputAdornment } from "@mui/material";
+import { TextField } from "@mui/material";
 
 const LeftContainer = styled.div`
   height: 100%;
   background-color: #fff;
   border-right: 2px solid #f3f3f5;
   margin-top: -2px;
+  .search-field {
+    fieldset {
+      display: none;
+    }
+  }
 `;
 
 function LeftPanel({ name }) {
-  return <LeftContainer>{name}</LeftContainer>;
+  return (
+    <LeftContainer>
+      <TextField
+        className="search-field"
+        type="search"
+        placeholder="Search"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </LeftContainer>
+  );
 }
 
 export default LeftPanel;

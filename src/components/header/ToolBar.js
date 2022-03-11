476ad174ed3958b26a4a6d4a6c399836da92e7ba
Cristina Lester <cristina.deelester@gmail.com>
2022-03-10 16:18:39 -0800

    added toolbar icons

--- src/components/header/ToolBar.js
+++ src/components/header/ToolBar.js
@@ -1,6 +1,9 @@
+import { Badge, IconButton } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import React from "react";
-import { IconButton } from "@mui/material";
-import styled from "react";
+import styled from "styled-components";
 
 const ToolBarContainer = styled.div`
   display: flex;
@@ -9,14 +12,53 @@ const ToolBarContainer = styled.div`
 
 const ButtonGroup = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .icon-button {
+    background: rgba(0, 0, 0, 0.04);
+    margin: 3px;
+    transition: background ease 500ms;
+    &:hover {
+      background: rgba(0, 0, 0, 0.08);
+    }
+  }
+`;
+
+const Title = styled.h1`
+  padding: 0 20px;
 `;
 
 function ToolBar(props) {
   return (
     <ToolBarContainer>
-      <h1>Dashboard</h1>
+      <Title>Dashboard</Title>
       <ButtonGroup>
-        <IconButton></IconButton>
+        <IconButton
+          className="icon-button"
+          size="large"
+          aria-label="calendar"
+          color="inherit"
+        >
+          <CalendarTodayIcon />
+        </IconButton>
+        <IconButton
+          className="icon-button"
+          size="large"
+          aria-label="messages"
+          color="inherit"
+        >
+          <ChatBubbleOutlineIcon />
+        </IconButton>
+        <IconButton
+          className="icon-button"
+          size="large"
+          aria-label="show 4 new mails"
+          color="inherit"
+        >
+          <Badge badgeContent={1} color="primary">
+            <NotificationsNoneIcon />
+          </Badge>
+        </IconButton>
       </ButtonGroup>
     </ToolBarContainer>
   );

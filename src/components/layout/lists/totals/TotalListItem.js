33025340d1d0e02a05f28183cf24c76052df32aa
Cristina Lester <cristina.deelester@gmail.com>
2022-03-12 08:37:52 -0800

    added totals list

---
+++ src/components/layout/lists/totals/TotalListItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const TotalLi = styled.li`
+  flex: 25%;
+  .card {
+    border-radius: 10px;
+    border: 2px solid #f3f3f5;
+    box-shadow: 4px 4px 20px #eee;
+    padding: 10px;
+    .card-content {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
+
+function TotalListItem({ totalItem }) {
+  const { amount, type, percentage, icon } = totalItem;
+
+  return (
+    <TotalLi className="total">
+      <Card className="card last-shipped-card" sx={{ minWidth: 320 }}>
+        <CardContent className="card-content">
+          <div className="total_header">
+            <FontAwesomeIcon className="total_header_icon" icon={icon} />
+            <p className="total_header_percentage">{percentage}</p>
+          </div>
+          <div className="total_footer">
+            <h4 className="total_footer_amount">{amount}</h4>
+            <p className="total_footer_type">Total {type}</p>
+          </div>
+        </CardContent>
+      </Card>
+    </TotalLi>
+  );
+}
+
+export default TotalListItem;
--- src/components/layout/split-screen/CenterPanel.js
+++ src/components/layout/split-screen/CenterPanel.js
@@ -1,14 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 
+import List from "../lists/List";
+import { totals } from "../../../resources";
+import TotalListItem from "../lists/totals/TotalListItem";
+
 const CentralConteriner = styled.div`
   height: 100%;
   background-color: #f9f9fb;
   padding: 40px;
+  .totals {
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px;
+    }
+  }
 `;
 
 function CenterPanel({ name }) {
-  return <CentralConteriner>{name}</CentralConteriner>;
+  return (
+    <CentralConteriner>
+      <div className="totals">
+        <List
+          items={totals}
+          resourceName="totalItem"
+          itemComponent={TotalListItem}
+        />
+      </div>
+    </CentralConteriner>
+  );
 }
 
 export default CenterPanel;
